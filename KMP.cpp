5cf8f960fc132d48e69ad470287eb1cc4bd71cf5
saurabhvashisth <vashisthsaurabh96@gmail.com>
2017-08-19 12:56:08 +0530

    Add KMP

--- KMP.cpp
+++ KMP.cpp
@@ -4,30 +4,30 @@ using namespace std;
 
 const int N = (int)1e5+11;
 string txt,pat;
-int lps[N];
+int lps[N],n,m;
 
-void preprocess(void){
-	int len=0,i=1,m=pat.size();
+void failure(void){
+	int len=0,i=1;
 	while(i<m){
 		while(i<m && pat[i]==pat[len]) lps[i++] = ++len;
-		if(i<m) len?len=lps[len-1]:i++;
+		len?len=lps[len-1]:i++;
 	}
 }
 
 void KMP(void){
-	int m = pat.size() , n = txt.size();
-	preprocess();
+	failure();
 	int i=0,j=0;
 	while(i<n){
-		while(i<n && j<m && pat[j]==txt[i]) j++ , i++;
+		while(i<n && j<m && pat[j]==txt[i]) j++,i++;
 		if(j==m) j = lps[j-1] , cout<<i-m<<endl; //pattern at i-m
-		else if(i<n) j?j=lps[j-1]:i++;
+		else j?j=lps[j-1]:i++;
 	}
 }
 
 int main()
 {
 	cin>>txt>>pat;
+	m=pat.size(),n=txt.size();
 	KMP();
 	return 0;
 }
